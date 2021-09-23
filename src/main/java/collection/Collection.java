8cfe6b486c66d2cb2867c55e90ab2f18d4dc3206
Oleksandr_Voinalovic <Oleksandr_Voinalovich@epam.com>
2021-09-23 15:52:32 +0300

    fix

--- src/main/java/collection/Collection.java
+++ src/main/java/collection/Collection.java
@@ -1,56 +1,31 @@
 package collection;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.InputMismatchException;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Collection {
 
     public static void main(String[] args) {
+        int size = 0;
         System.out.println("Enter size: ");
         Scanner input = new Scanner(System.in);
-        int size = input.nextInt();
-        List<Integer> inputList = new ArrayList<>(size);
+        try {
+            size = input.nextInt();
+        } catch (InputMismatchException x) {
+            System.out.println("Enter correct size");
+        }
         Set<Integer> inputSet = new HashSet<>(size);
         System.out.println("Enter elements:");
-        Set<Integer> set = inputUniqueData(input, size, inputSet);
-        printData(set);
-        System.out.println("Enter next elements: ");
-        Set<Integer> set1 = getUniqueValues(input, size, inputList);
-        printData(set1);
-    }
-
-    private static Set<Integer> getUniqueValues(Scanner input, int size, List<Integer> inputList) {
         try {
             for (int i = 0; i < size; i++) {
-                inputList.add(input.nextInt());
+                inputSet.add(input.nextInt());
             }
         } catch (InputMismatchException e) {
             System.out.printf("it`s not a number");
         }
-        Set<Integer> set = new HashSet<>(inputList);
-        return set;
-    }
-
-    private static Set<Integer> inputUniqueData(Scanner input, int size, Set<Integer> inputSet) {
-        try {
-            for (int i = 0; i < size; i++) {
-                if (!inputSet.add(input.nextInt())) {
-                    size++;
-                    System.out.println("write next number");
-                }
-            }
-        } catch (InputMismatchException e) {
-            System.out.printf("it`s not a number");
-        }
-        return inputSet;
-    }
-
-    private static void printData(Set<Integer> inputSet) {
         Iterator<Integer> iterator = inputSet.iterator();
         while (iterator.hasNext()) {
             System.out.println(iterator.next());
