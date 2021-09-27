5594076174af0cee16dca42bae2118eb074135c1
Oleksandr_Voinalovic <Oleksandr_Voinalovich@epam.com>
2021-09-27 15:41:23 +0300

    fix

--- src/main/java/collection/Collection.java
+++ src/main/java/collection/Collection.java
@@ -14,21 +14,19 @@ public class Collection {
         int size = 0;
         System.out.println("Enter size: ");
         Scanner input = new Scanner(System.in);
-
-        try {
-            size = input.nextInt();
-        } catch (InputMismatchException x) {
+        while (!input.hasNextInt()) {
             System.out.println("Enter correct size");
-            return;
+            input.next();
         }
+        size = input.nextInt();
         if (size <= 0) {
-            throw new CollectionSizeException("size must be >0");
+            throw new CollectionSizeException("Size must be > 0");
         }
         Set<Integer> inputSet = new HashSet<>(size);
-        System.out.println("Enter elements:");
+        System.out.println("Enter elements: ");
         for (int i = 0; i < size; i++) {
             while (!input.hasNextInt()) {
-                System.out.println("it`s not a number");
+                System.out.println("It`s not a number");
                 input.next();
             }
             inputSet.add(input.nextInt());
@@ -37,14 +35,19 @@ public class Collection {
         while (iterator.hasNext()) {
             System.out.println(iterator.next());
         }
-        System.out.printf("MaxElement is:");
-        int maxElement = inputSet.stream().max(Integer::compare).get();
-        System.out.println(maxElement);
-        System.out.println("sum :");
-        int suma = inputSet.stream().mapToInt(Integer::intValue).filter(x -> x % 2 == 0).sum();
-        System.out.println(suma);
-        System.out.println("revers");
-        Stream<Integer> revers = inputSet.stream().sorted(Collections.reverseOrder());
+
+        System.out.println("MaxElement is :" + inputSet.stream()
+                .max(Integer::compare)
+                .get());
+
+        System.out.println("sum :" + inputSet.stream()
+                .mapToInt(Integer::intValue)
+                .filter(x -> x % 2 == 0)
+                .sum());
+
+        System.out.println("revers :");
+        Stream<Integer> revers = inputSet.stream()
+                .sorted(Collections.reverseOrder());
         Iterator<Integer> reversIterator = revers.iterator();
         while (reversIterator.hasNext()) {
             System.out.println(reversIterator.next());
